43c22bb4c277282d86c9add121daeb6d604cec91
ranjeetsaini95 <ranjeetsaini9599@gmail.com>
2026-03-10 01:36:05 +0530

    Create amazon_oauth.js

---
+++ amazon_oauth.js
@@ -0,0 +1,16 @@
+import { AMAZON_CLIENT_ID } from "./config.js"
+
+window.connectAmazon = function(){
+
+  const redirectUri = window.location.origin + "/callback.html"
+
+  const url =
+  "https://www.amazon.com/ap/oa?" +
+  "client_id=" + AMAZON_CLIENT_ID +
+  "&scope=advertising::campaign_management" +
+  "&response_type=code" +
+  "&redirect_uri=" + encodeURIComponent(redirectUri)
+
+  window.location.href = url
+
+}
