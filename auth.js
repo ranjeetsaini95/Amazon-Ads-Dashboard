9fc937babe6a1614db162be2e5faabcb63f635ac
ranjeetsaini95 <ranjeetsaini9599@gmail.com>
2026-03-10 00:44:41 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,42 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
+const supabase = createClient(
+'https://YOUR_PROJECT.supabase.co',
+'YOUR_ANON_KEY'
+)
+
+async function signup(){
+
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
+
+const { error } = await supabase.auth.signUp({
+email,
+password
+})
+
+if(error){
+alert(error.message)
+}else{
+alert("Account created")
+}
+
+}
+
+async function login(){
+
+const email = document.getElementById("email").value
+const password = document.getElementById("password").value
+
+const { error } = await supabase.auth.signInWithPassword({
+email,
+password
+})
+
+if(error){
+alert(error.message)
+}else{
+window.location.href = "dashboard.html"
+}
+
+}
