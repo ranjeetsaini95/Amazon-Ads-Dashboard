7eba12aa1a57fb08d01781b256bb440e6b871624
ranjeetsaini95 <ranjeetsaini9599@gmail.com>
2026-03-10 00:58:22 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,42 +1,45 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./config.js"
 
 const supabase = createClient(
-'https://bhwwiyrxhvrscnhzvych.supabase.co',
-'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJod3dpeXJ4aHZyc2NuaHp2eWNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3MzM0NTgsImV4cCI6MjA4ODMwOTQ1OH0.6Cl-U5HuzFs2A7leRBBmz7514RtPEtCcWJLcUrbLnV4'
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 )
 
-async function signup(){
+window.signup = async function(){
 
-const email = document.getElementById("email").value
-const password = document.getElementById("password").value
+  const email = document.getElementById("email").value
+  const password = document.getElementById("password").value
 
-const { error } = await supabase.auth.signUp({
-email,
-password
-})
+  const { data, error } = await supabase.auth.signUp({
+    email: email,
+    password: password
+  })
 
-if(error){
-alert(error.message)
-}else{
-alert("Account created")
+  if(error){
+    alert(error.message)
+  } else {
+    alert("Signup successful. You can now login.")
+  }
+
 }
 
-}
+window.login = async function(){
 
-async function login(){
+  const email = document.getElementById("email").value
+  const password = document.getElementById("password").value
 
-const email = document.getElementById("email").value
-const password = document.getElementById("password").value
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email: email,
+    password: password
+  })
 
-const { error } = await supabase.auth.signInWithPassword({
-email,
-password
-})
+  if(error){
+    alert(error.message)
+  } else {
 
-if(error){
-alert(error.message)
-}else{
-window.location.href = "dashboard.html"
-}
+    window.location.href = "dashboard.html"
+
+  }
 
 }
