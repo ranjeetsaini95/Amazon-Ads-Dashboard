fde7da70f1a3d6357a187de86a6021a687984ed4
ranjeetsaini95 <ranjeetsaini9599@gmail.com>
2026-03-10 01:40:41 +0530

    Update amazon_oauth.js

--- amazon_oauth.js
+++ amazon_oauth.js
@@ -5,12 +5,13 @@ window.connectAmazon = function(){
   const redirectUri = window.location.origin + "/callback.html"
 
   const url =
-  "https://www.amazon.com/ap/oa?" +
-  "client_id=" + AMAZON_CLIENT_ID +
-  "&scope=advertising::campaign_management" +
-  "&response_type=code" +
-  "&redirect_uri=" + encodeURIComponent(redirectUri)
+    "https://www.amazon.com/ap/oa?" +
+    "client_id=" + AMAZON_CLIENT_ID +
+    "&scope=advertising::campaign_management" +
+    "&response_type=code" +
+    "&redirect_uri=" + encodeURIComponent(redirectUri)
+
+  console.log("Redirecting to Amazon OAuth:", url)
 
   window.location.href = url
-
 }
